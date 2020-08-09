f28bae684e70a635102dba9f5a34398ae76561b1
Will Kotheimer <wkotheimer@gmail.com>
2020-08-08 23:31:58 -0500

    Updated UX

--- project.js
+++ project.js
@@ -22,6 +22,7 @@ const projects = [
 
 ];
 
+const backgroundsForCards = ['#94bcc4', '#c36f31', '#dda982', '#b9cb99', '#b7bf5e', '#076e54']
 // printToDom: This function should accept 2 arguments: 
 // the ID of the element to print it in and a string to print.
 
@@ -33,22 +34,26 @@ const printToDom = (id, stringToPrint) => {
 //through the projects array and build up a 'dom string' 
 
 const createProjectCards = (projectsForCards) => {
-    let myString = `<header class="projects">Some of My Projects:</header>`;
+    let myString = `<header class="headerspecial">Some of My Projects:</header><span style='border: 1px solid white; width:100px;'></span>
+    <div class="projectsContainer">`;
     for (let x = 0; x < projectsForCards.length; x++) {
         if (projectsForCards[x].available !== true) {
             continue;
         } else {
             myString += `
-            
-            <div class="title">${projectsForCards[x].title}</div>
+            <div class="projectCard" style="background-color:${backgroundsForCards[x % 6]};">
             <div class="screenshot"><img class='project' src='${projectsForCards[x].screenshot}'/></div>
+            <div class="title">${projectsForCards[x].title}</div>
+            
             <div class="description">${projectsForCards[x].description}</div>
             <div class="technologiesUsed">${projectsForCards[x].technologiesUsed}</div>
             <div class=" url"><a href='${projectsForCards[x].url}'>Pet Adoption Site</a></div>
             <div class="githubUrl"><a href='${projectsForCards[x].githubUrl}'>https://github.com/willkotheimer/pet-adoption</a></div>
+            </div>
             `
         }
     }
+    myString += "</div>"
     printToDom('projectsPage', myString);
 }
 
@@ -57,16 +62,16 @@ function onDragStart(event) {
 
     console.log(event.target.id);
     console.log(event.target);
-    event.currentTarget.style.backgroundColor = "grey";
+    event.currentTarget.style.backgroundColor = "#1F1E4D";
     event.currentTarget.myString = event.target.id;
 }
 
 function onDragOver(event) {
     event.preventDefault();
-    event.currentTarget.style.height = "86%";
+    event.currentTarget.style.height = "300px";
     event.currentTarget.style.width = "91%";
-    event.currentTarget.style.backgroundColor = "rgba(148, 147, 213, 0.8)";
-    event.currentTarget.style.border = "10px solid rgba(148, 147, 213, 0.8)";
+    event.currentTarget.style.backgroundColor = "rgba(148, 188, 196, 0.8)";
+    event.currentTarget.style.border = "10px solid rgba(148, 188, 196, 0.8)";
 }
 
 let previousDaggableElement;
@@ -75,12 +80,12 @@ function onDrop(event) {
     const id = event.dataTransfer.getData("text/plain");
     const draggableElement = document.getElementById(id);
     const dropzone = event.target;
-    draggableElement.style.backgroundColor = "red";
-    if (previousDaggableElement && previousDaggableElement != draggableElement) { previousDaggableElement.style.backgroundColor = "grey"; }
+    draggableElement.style.backgroundColor = "slategrey";
+    if (previousDaggableElement && previousDaggableElement != draggableElement) { previousDaggableElement.style.backgroundColor = "rgb(0, 22, 40)"; }
     previousDaggableElement = document.getElementById(id);
-    event.currentTarget.style.height = "84%";
+    event.currentTarget.style.height = "300px";
     event.currentTarget.style.width = "89%";
-    event.currentTarget.style.backgroundColor = "rgba(61, 60, 153, 1)";
+    event.currentTarget.style.backgroundColor = "rgb(0, 22, 40);";
     event.currentTarget.style.border = "0px";
 
 
@@ -90,54 +95,101 @@ function onDrop(event) {
     let description = "";
     switch (id) {
         case "onin":
-            description = `<div id="onin">(2016-2017) Onin Group - PHP Developer
-            </div><div><ul><li>Frontend coded momentumcapitalfunding.com (Wordpress) and backend coded major release for oninapps.com timekeeping software (PHP), involving end-to-end design to deployment.</li>   
-            <li>Worked with internal facing customers such as operations team to define issues with system, and also collaborated to create a way of capturing and testing new environment.</li>  
-            </ul> 
-            </div>`;
+            description = `<div id="onin-description">(2016-2017) Onin Group - PHP Developer
+            </div><div>Highlights:</div>
+            <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_developer_1018327.png'/></div>
+            <div id="experience-description">
+                <ul>
+                    <li>Frontend coded momentumcapitalfunding.com (Wordpress).
+                    <li> Backend coded major release for oninapps.com timekeeping software (PHP), involving computing monthly,
+                    weekly, and daily results of reports of each employee, department, branch, and region.</li>
+                    <li>Worked with operations team to define issues with system, and collaborated to test new environment.</li>
+                </ul>
+            </div>
+            </div>
+        </div>`;
             break;
         case "advance":
-            description = `<div id="advance">(2017-2018) Advance Financial -
-            Customer Service</div><div<ul><li>Provided customer support and sold benefits.</li>   
+            description = `<div id="advance-description">(2017-2018) Advance Financial -
+            Customer Service</div><div>Highlights:</div>
+            <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_customer service_847498.png'/></div>
+            <div id="experience-description"><ul><li>Provided customer support and sold benefits.</li>   
             <li>Entered call notes to record conversation history.</li>   
             </ul>
-            </div>`;
+            </div></div></div>`;
             break;
         case "view360":
-            description = `<div id="view360">(2019-Present) 360 View - Junior
-            Quality Assurance</div><div>
+            description = `<div id="view360-description">(2019-Present) 360 View - Junior
+            Quality Assurance</div><div>Highlights:</div>
+            <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_Final Approval_995760.png'/></div>
+            <div id="experience-description">
             <ul><li>Participates in SDLC: Grooming, Scrum, Retro, and Kickoff. </li>                          
             <li>Implements Manual QA testing for Banking CRM, iOS and Android mobile, and SQL Server Sprocs. </li>  
             <li>Keeps wiki updated with repro.</li>  
             <li>Creates Scenarios in Specflow (Gerkhin) language for automation with Acceptance Criteria.</li>  
             <li>Creates Automated tests with Selenium and C#.</li>  
             </ul>
-            </div>`;
+            </div></div></div>`;
             break;
         case "acxiom":
-            description = `<div id="acxiom">(2015-2016) Acxiom - Email Developer
-            </div><div><ul><li>Worked directly with Sam's Club, CTFS, and Sallie Mae marketing. </li>  
+            description = `<div id="acxiom-description">(2015-2016) Acxiom - Email Developer
+            </div><div>Highlights:</div>
+            <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_Email Marketing_3188032.png'/></div>
+            <div id="experience-description"><ul><li>Worked directly with Sam's Club, CTFS, and Sallie Mae marketing. </li>  
             <li>Provided service to coworkers by helping with email QAs.</li>   
             <li>Prioritized work that including immediate launch and up to five day turnarounds. </li>  
-            </div>`;
+            </div></div></div>`;
             break;
         case "jackroe":
-            description = `<div id="jackroe">(2018-2019) Jack Roe USA - Tech Support
-            and QA</div><div><ul><li>Implemented Manual QA testing for web, desktop, mobile, and command line systems.
+            description = `<div id="jackroe-description">(2018-2019) Jack Roe USA - Tech Support
+            and QA</div><div>Highlights:</div>
+            <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_tech support_368088.png'/></div>
+            <div id="experience-description"><ul><li>Implemented Manual QA testing for web, desktop, mobile, and command line systems.
             <li> Utilized spec to draw up use cases in excel, copying database data, and reporting findings back to developer. </li>  
             <li>Deployed IOS and Android apps, and set up Google Analytics dashboard.</li>        
             <li>Created Jira Specs including db tables, function, and UI. </li>  
-            </div>`;
+            </div></div></div>`;
             break;
         case "nss":
-            description = `<div>.Net and React</div>`;
-            break;
-        case "wku":
-            description = `<div id="wku">
+            description = `<div id="nss-description">
             <div>(2010-2014) Western Kentucky University</div>
             <div>Computer Science / Minor in Advertising</div>
 
-        </div><div>Highlights:
+        </div><div>Highlights:</div>
+        <div id="containerSurround">
+            <div id="experience-logo"><img src='images/icons/noun_education_1013174.png'/></div>
+            <div id="experience-description">
+            <ul>
+                <li>.Net and React</li>
+                <li>Javascript Single Page Apps</li>
+                <li>Teambuilding within SDLC</li>
+                <li>Weekly, Midterm and Final Projects</li>
+            </ul>
+            </div>
+            </div>
+           
+        </div>`;
+            break;
+        case "wku":
+            description = `<div id="wku-description">
+            <div>(2010-2014) Western Kentucky University</div>
+            <div>Computer Science / Minor in Advertising</div>
+
+        </div><div>Highlights:</div>
+        <div id="containerSurround">
+            
+            <div id="experience-logo"><img src='images/icons/noun_education_1013174.png'/></div>
+            <div id="experience-description">
             <ul>
                 <li>Student Developer: Potter College</li>
                 <li>Intern: Imagewest</li>
@@ -145,6 +197,9 @@ function onDrop(event) {
                 <li>Student Assistant: WKU Networking Department</li>
                 <li>Student Developer: WKU Library</li>
             </ul>
+            </div>
+            </div>
+           
         </div>`;
             break;
         default: "djshdjsa";
