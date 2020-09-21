fcf3a554585d8576a45afb0370e8fcd3d5d7c228
wkotheimer <wkotheimer@gmail.com>
2020-09-21 00:26:04 -0500

    Finished project clicker

--- src/javascripts/project.js
+++ src/javascripts/project.js
@@ -1,6 +1,6 @@
 import projects from './helpers/data/projectsData';
 
-const backgroundsForCards = ['#94bcc4', '#c36f31', '#dda982', '#b9cb99', '#b7bf5e', '#076e54'];
+// const backgroundsForCards = ['#94bcc4', '#c36f31', '#dda982', '#b9cb99', '#b7bf5e', '#076e54'];
 // printToDom: This function should accept 2 arguments:
 // the ID of the element to print it in and a string to print.
 
@@ -9,27 +9,22 @@ const printToDom = (selector, stringToPrint) => {
   handle.innerHTML = stringToPrint;
 };
 
-const createProjectCards = () => {
-  let myString = `<header class='headerspecial'>Some of My Projects:</header>
-    <div class='projectsContainer'>`;
-  // for (let x = 0; x < projectsForCards.length; x += 1) {
-  //   if (projectsForCards[x].available !== true) {
-  //     continue;
-  //   } else {
-  //     myString += `
-  //           <div class='projectCard' style='background-color:${backgroundsForCards[x % 6]};'>
-  //           <div class='screenshot'><img class='project' src='${projectsForCards[x].screenshot}'/></div>
-  //           <div class='title'>${projectsForCards[x].title}</div>
+const createProjectCards = (idString) => {
+  $('.inner-project').html('');
+  console.warn(projects.getProjects());
+  const id = Number(idString.split('-')[1]);
+  const myString = `
+            <div class='projectCard' style='background-color:#94bcc4'>
+            <div class='screenshot project-${id}'></div>
+            <div class='title'>${projects.getProjects()[id].title}</div>
 
-  //           <div class='description'>${projectsForCards[x].description}</div>
-  //           <div class='technologiesUsed'>${projectsForCards[x].technologiesUsed}</div>
-  //           <div class='url'><a href='${projectsForCards[x].url}'>Pet Adoption Site</a></div>
-  //           <div class='githubUrl'><a href='${projectsForCards[x].githubUrl}'>https://github.com/willkotheimer/pet-adoption</a></div>
-  //           </div>
-  //           `
-  //   }
-  // }
-  // myString += '</div>
+            <div class='description'>${projects.getProjects()[id].description}</div>
+            <div class='technologiesUsed'>${projects.getProjects()[id].technologiesUsed}</div>
+            <div class='url'><a href='${projects.getProjects()[id].url}'>Pet Adoption Site</a></div>
+            <div class='githubUrl'><a href='${projects.getProjects()[id].githubUrl}'>https://github.com/willkotheimer/pet-adoption</a></div>
+            </div>
+            `;
+  $('.inner-project').html(myString);
 };
 
 const addButtons = () => {
@@ -44,8 +39,7 @@ const addEvents = () => {
   projects.getProjects().forEach((project, index) => {
     document.querySelector(`#add-${index}`)
       .addEventListener('click', (e) => {
-        const id = e.target.id;
-        createProjectCards('.inner-project', createProjectCards, id);
+        createProjectCards(e.target.id);
       });
   });
 };
